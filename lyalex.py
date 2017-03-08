7b77fb5be680a2e54951ad621ab726f14806dc4f
Leo Yuuki Omori Omi <leoyuuki@gmail.com>
2017-03-08 15:58:50 -0300

    Add reserved word list

--- lyalex.py
+++ lyalex.py
@@ -5,26 +5,36 @@ import ply.lex as lex
 
 # List of token names.   This is always required
 tokens = (
-           'NUMBER',
-            'PLUS',
-            'MINUS',
-            'TIMES',
-            'DIVIDE',
-            'LPAREN',
-            'RPAREN',
-         )
+        'NUMBER',
+        'PLUS',
+        'MINUS',
+        'TIMES',
+        'DIVIDE',
+        'LPAREN',
+        'RPAREN',
+)
 
 reserved = {
-    'array': 'ARRAY',
-    'by': 'BY',
-    'chars': 'CHARS', 
-    'dcl': 'DCL', 
-    'do': 'DO', 
-    DOWN, ELSE, ELSIF, END, EXIT, FI,
-FOR, IF, IN, LOC, TYPE, OD, PROC, REF, RESULT, RETURN, RETURNS,
-SYN, THEN, TO, WHILE, ABS, ASC, BOOL, CHAR, FALSE, INT, LENGTH, LOWER, NULL, NUM,
-PRINT, READ, TRUE, UPPER
-
+        'array': 'ARRAY',
+        'by': 'BY',
+        'chars': 'CHARS', 
+        'dcl': 'DCL', 
+        'do': 'DO', 
+        'down': 'DOWN',
+        'else': 'ELSE',
+        'elsif': 'ELSIF',
+        'end': 'END',
+        'exit': 'EXIT,
+        'fi': 'FI',
+        'for': 'FOR',
+        'if': 'IF', 
+        'in': 'IN', 
+        'loc': 'LOC',
+        'type': 'TYPE',
+        'od': 'OD',
+        'proc': 'PROC', REF, RESULT, RETURN, RETURNS,
+        SYN, THEN, TO, WHILE, ABS, ASC, BOOL, CHAR, FALSE, INT, LENGTH, LOWER, NULL, NUM,
+        PRINT, READ, TRUE, UPPER
 }
 
 # Regular expression rules for simple tokens
@@ -38,13 +48,13 @@ t_RPAREN  = r'\)'
 # A regular expression rule with some action code
 def t_NUMBER(t):
         r'\d+'
-            t.value = int(t.value)    
+        t.value = int(t.value)    
                 return t
 
             # Define a rule so we can track line numbers
             def t_newline(t):
                     r'\n+'
-                        t.lexer.lineno += len(t.value)
+                    t.lexer.lineno += len(t.value)
 
                         # A string containing ignored characters (spaces and tabs)
                         t_ignore  = ' \t'
@@ -52,7 +62,7 @@ def t_NUMBER(t):
                         # Error handling rule
                         def t_error(t):
                                 print("Illegal character '%s'" % t.value[0])
-                                    t.lexer.skip(1)
+                                t.lexer.skip(1)
 
                                     # Build the lexer
                                     lexer = lex.lex()
