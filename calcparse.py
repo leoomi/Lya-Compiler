0e7cb710d57190bd4daf0314ccf366eae94ecf4e
Rogerio Bernardo <roger13@localhost.localdomain>
2017-06-14 17:35:02 -0300

    Added print and read value, changed the recursion for code generation

--- calcparse.py
+++ calcparse.py
@@ -11,28 +11,8 @@ code = []
 
 def vis(node):
     global refs, sp, code
-    print(node)
-    if isinstance(node, Program):
-        code.append(('stp',))
-
-    elif isinstance(node, Declaration):
-        if isinstance(node.mode, DiscreteMode):
-            varSize = 1
-        else:
-            #GET ARRAY AND STRING LEN
-            varSize = 1
-        code.append(('acl', varSize * len(node.identifier_list)))
-
-        #add reference to stack
-        for i in node.identifier_list:
-            refs[i] = sp
-            sp += 1
-        
-        if isinstance(node.initialization, Constant):
-            #se tiver inicializacao fazer LDC + STR
-
-
-    #visit recursively
+    
+    #visit recursively (DFS)
     if hasattr(node,"_fields"):
         for field in getattr(node,"_fields"):
             print (field)
@@ -46,6 +26,54 @@ def vis(node):
     else:
         print("Leaf node...")
 
+    
+    print(node)
+    if isinstance(node, Program):
+        code.append(('end',))
+
+    elif isinstance(node, Declaration):
+        if isinstance(node.mode, DiscreteMode):
+            varSize = 1
+        else:
+            #GET ARRAY AND STRING LEN
+            varSize = 1
+
+        code.append(('acl', varSize * len(node.identifier_list)))
+
+        #add reference to stack
+        for i in node.identifier_list:
+            refs[i.label] = sp
+            sp += 1
+        
+        if isinstance(node.initialization, Constant):
+            for i in node.identifier_list:
+                code.append(('ldc', node.initialization.value))
+                code.append(('stv','ALTERAR',refs[i.label]))
+
+    elif isinstance(node, BuiltinCall):
+        if node.builtin_name == "read":
+            for i in node.parameter_list:
+                #CHECK IF TYPE IS INT
+                if True:
+                    code.append(('rdv',))
+                    code.append(('stv','ALTERAR',refs[i.label]))
+                #CHECK IF TYPE IS STRING
+                else:
+                    code.append(('ldr', 'ALTERAR', 'ALTERAR'))
+                    code.append(('rds',))
+        elif node.builtin_name == "print":
+            for i in node.parameter_list:
+                #CHECK IF TYPE IS INT
+                if True:
+                    code.append(('prv', 0))
+                else:
+                    code.append(('prv', 1))
+    
+#    elif isinstance(node.initialization, BinaryOperation):
+ #       if node.operator == "*":
+
+
+
 
 while True:
     try:
@@ -57,8 +85,10 @@ while True:
     #visit = visitor.visit(result)
     
     print("--START--")
+    code.append(('stp',))
     vis(result)
     print(code)
-    print(refs)
+    for i in refs:
+        print(i, refs[i])
 
 
